f8df7c683c8122f695925f03944a00192ebc170d
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-06-17 15:25:27 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    insertNewelemnt()
+ 
+});
+
+insertNewelemnt = () => document.getElementById("text") .innerHTML = "This is really cool!";
